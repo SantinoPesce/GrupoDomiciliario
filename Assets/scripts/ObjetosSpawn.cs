8394ba714237277cbe66f9117d14895ee43d2537
SantinoPesce <147621509+SantinoPesce@users.noreply.github.com>
2024-06-26 09:06:58 -0300

    Cuarto(Spawn ready)

--- Assets/scripts/ObjetosSpawn.cs
+++ Assets/scripts/ObjetosSpawn.cs
@@ -6,15 +6,24 @@ public class ObjetosSpawn : MonoBehaviour
 {
     public GameObject[] Derecha;
     public GameObject[] Izquierda;
+    int RD;
+    int RI;
     // Start is called before the first frame update
     void Start()
     {
-        
+    RI = Random.Range(0, Izquierda.Length);
+    RD = Random.Range(0, Derecha.Length);
+        Valores();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void Valores()
+    {
+        Derecha[RD].SetActive(true);
+        Izquierda[RI].SetActive(true);
     }
 }
