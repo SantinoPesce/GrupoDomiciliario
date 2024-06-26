2fd7c57c2c9472509d491534ea6c41a675753e96
dantecanti <dantecantisano1@gmail.com>
2024-06-26 16:01:48 -0300

    octavo

--- Assets/ResultadosScript.cs
+++ Assets/ResultadosScript.cs
@@ -6,27 +6,43 @@ using UnityEngine.UI;
 public class ResultadosScript : MonoBehaviour
 {
     public GameObject panel;
-    public InputField txt;
+    public Text txt;
+    public ObjetosSpawn objetospawn;
 
+    public Text txtpanel;
 
     // Start is called before the first frame update
     void Start()
     {
         panel.SetActive(false);
         
+        
     }
 
     // Update is called once per frame
     void Update()
-    { 
-        
+    {
     }
+
     public void botonpresionado()
     {
       if (txt.text == "")
         {
             panel.SetActive(true);
+            txtpanel.text = "Debes ingresar un resultado";
+
         }
        
+     int total = objetospawn.Izquierda[objetospawn.RI].GetComponent<ObjetoValueScript>().Value + objetospawn.Derecha[objetospawn.RD].GetComponent<ObjetoValueScript>().Value;
+        if (total.ToString() == txt.text.ToString())
+        {
+            panel.SetActive(true);
+            txtpanel.text = "Resultado correcto";
+
+        }
+    }
+    public void panelsinresultado()
+    {
+        panel.SetActive(false);
     }
 }
