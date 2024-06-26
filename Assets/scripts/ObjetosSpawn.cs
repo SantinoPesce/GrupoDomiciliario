9cd45369bff26a5a473c8c88b58cdb10cbc2fe5d
SantinoPesce <147621509+SantinoPesce@users.noreply.github.com>
2024-06-26 10:43:36 -0300

    cinco (valores)

--- Assets/scripts/ObjetosSpawn.cs
+++ Assets/scripts/ObjetosSpawn.cs
@@ -1,25 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObjetosSpawn : MonoBehaviour
 {
     public GameObject[] Derecha;
     public GameObject[] Izquierda;
-    int RD;
-    int RI;
+    public int RD;
+    public int RI;
+
+    public Text txtDerecha;
+    public Text txtIzquierda;
+
     // Start is called before the first frame update
     void Start()
     {
-    RI = Random.Range(0, Izquierda.Length);
-    RD = Random.Range(0, Derecha.Length);
+        RI = Random.Range(0, Izquierda.Length);
+        RD = Random.Range(0, Derecha.Length);
         Valores();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        txtDerecha.text = "$ " + Derecha[RD].GetComponent<ObjetoValueScript>().Value;
+        txtDerecha.text = "$ " + Izquierda[RI].GetComponent<ObjetoValueScript>().Value;
     }
     public void Valores()
     {
