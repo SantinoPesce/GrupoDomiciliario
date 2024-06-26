a58887cbc3c1303b6517a504616a6e23fee54935
dantecanti <dantecantisano1@gmail.com>
2024-06-26 14:31:11 -0300

    septimo

---
+++ Assets/ResultadosScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultadosScript : MonoBehaviour
+{
+    public GameObject panel;
+    public InputField txt;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        
+    }
+    public void botonpresionado()
+    {
+      if (txt.text == "")
+        {
+            panel.SetActive(true);
+        }
+       
+    }
+}
